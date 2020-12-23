a54d16116243a0d3c270368e2a59f5e33a4fa56d
Mad Zephyr <v21400@gmail.com>
2020-12-23 14:28:41 +0200

    Jquerry added

---
+++ src/js/script.js
@@ -0,0 +1,26 @@
+
+$(function() {
+    $("#price-range").slider({
+        range: "max",
+        min: 1000,
+        max: 70000,
+        value: 1,
+        slide: function( event, ui ) {
+        $( "#priceRange" ).val( ui.value );
+        }
+    });
+    $("#priceRange").val(  $("#price-range").slider("values", 0)+"$" + " - $" + $("#price-range").slider("values", 1));
+} );
+
+// $(function() {
+//     $("#price-range").slider({
+//     range: true,
+//     min: 0,
+//     max: 200000,
+//     values: [0, 200000],
+//     slide: function(event, ui) {
+//         $("#priceRange").val("$" + ui.values[0] + " - $" + ui.values[1]);
+//     }
+//     });
+//     $("#priceRange").val("$" + $("#price-range").slider("values", 0) + " - $" + $("#price-range").slider("values", 1));
+// }
